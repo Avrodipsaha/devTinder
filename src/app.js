7067bf8e1c612d65fe78394038fe11e4e7ef2102
Avrodip Saha <avrodip1111@gmail.com>
2025-02-23 02:27:12 +0530

    playing aroud roughts

--- src/app.js
+++ src/app.js
@@ -2,25 +2,55 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({ name: "Avrodip", age: 30 });
-});
+// ? is used for optional and terget the string which is present before the ? symbol
+// app.get("/ab?c", (req, res) => {
+//   console.log(req.params);
+//   res.send({ name: "Avrodip", age: 20 });
+// });
 
-app.post("/user", (req, res) => {
-  res.send("Data has been saved successfully");
-});
+// In place of * use can use any string in url which give same result
+// app.get("/ab*c", (req, res) => {
+//   console.log(req.params);
+//   res.send({ name: "Avrodip", age: 23 });
+// });
 
-app.put("/user", (req, res) => {
-  res.send("Overwrite the data successfully");
-});
+// using + symbol we can target the string which is present before the + symbol use the string multiple times in the url which give same result
+// app.get("/ab+c", (req, res) => {
+//   console.log(req.params);
+//   res.send({ name: "Saha", age: 20 });
+// });
 
-app.patch("/user", (req, res) => {
-  res.send("Data is updated successfully");
-});
+//using of () we can target the sting inside the bracket
+// app.get("/ab(dd)?c", (req, res) => {
+//   console.log(req.params);
+//   res.send({ name: "Avrodip", age: 30 });
+// });
 
-app.delete("/user", (req, res) => {
-  res.send("Data is deleted successfully");
-});
+// Dynamic routing
+// app.get("/user/:id/:name", (req, res) => {
+//   console.log(req.params);
+//   res.send({ name: "Avrodip", age: 30 });
+// });
+
+// app.get("/user", (req, res) => {
+//   res.send({ name: "Avrodip", age: 30 });
+// });
+
+// app.post("/user", (req, res) => {
+//   res.send("Data has been saved successfully");
+// });
+
+// app.put("/user", (req, res) => {
+//   res.send("Overwrite the data successfully");
+// });
+
+// app.patch("/user", (req, res) => {
+//   res.send("Data is updated successfully");
+// });
+
+// app.delete("/user", (req, res) => {
+//   res.send("Data is deleted successfully");
+// });
 
 // app.use is handale all type of requests
 // app.use("/backend", (req, res) => {
